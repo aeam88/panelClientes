9c74536ad92aeccca1b1254ac1e8a895958d2af2
Alejandro <a.alvarezmontes@gmail.com>
2018-08-16 16:59:04 -0300

    Proyecto Panel Clientes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBgUcaFn3QeW-i4R1KM0wnIA7v2Nwg9_8U",
+    authDomain: "clientpanelprod-7d798.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-7d798.firebaseio.com",
+    projectId: "clientpanelprod-7d798",
+    storageBucket: "clientpanelprod-7d798.appspot.com",
+    messagingSenderId: "1085256816185"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBgUcaFn3QeW-i4R1KM0wnIA7v2Nwg9_8U",
+    authDomain: "clientpanelprod-7d798.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-7d798.firebaseio.com",
+    projectId: "clientpanelprod-7d798",
+    storageBucket: "clientpanelprod-7d798.appspot.com",
+    messagingSenderId: "1085256816185"
+  }
 };
 
 /*
