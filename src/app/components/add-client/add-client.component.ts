cd45f0707925ce30144614b126736615f85d3935
Alejandro <a.alvarezmontes@gmail.com>
2018-08-17 12:36:20 -0300

    Add Cliente y detalles

--- src/app/components/add-client/add-client.component.ts
+++ src/app/components/add-client/add-client.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { ClientService } from '../../services/client.service';
+import { Router } from '@angular/router';
+
+import { Client } from '../../models/Client';
 
 @Component({
   selector: 'app-add-client',
@@ -6,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-client.component.css']
 })
 export class AddClientComponent implements OnInit {
+  client: Client = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    balance: 0,
+  }
 
-  constructor() { }
+  disableBalanceOnAdd: boolean = true;
+  @ViewChild('clientForm') form: any;
+
+  constructor(
+    private flashMessage: FlashMessagesService, 
+    private clientService: ClientService,
+    private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onSubmit({value, valid}: {value: Client, valid:boolean}) {
+    if(this.disableBalanceOnAdd) {
+      value.balance = 0;
+    }
+
+    if(!valid) {
+      this.flashMessage.show('Por favor llenar el formulario correctamente', {
+        cssClass: 'alert-danger', timeout: 4000
+      });
+    } else {
+
+      this.clientService.newClient(value);
+
+      this.flashMessage.show('Cliente añadido correctamente', {
+        cssClass: 'alert-success', timeout: 4000
+      });
+      this.router.navigate(['/']);
+    }
   }
 
 }
